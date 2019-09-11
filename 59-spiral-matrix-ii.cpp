3218986913515a27ab2e92c3264075deb0e55e95
YanZhao <whuhenry@gmail.com>
2019-09-11 21:31:13 +0800

    add solution 59

--- 59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii.cpp
@@ -4,19 +4,18 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> result;
-        int rows = matrix.size();
-        if (0 == rows) {
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> result;
+        if (0 == n) {
             return result;
         }
-        int cols = matrix[0].size();
-        if (0 == cols) {
-            return result;
+        result.resize(n);
+        for (int i = 0; i < n; ++i) {
+            result[i].resize(n);
         }
-        int count = rows * cols;
+        int count = n * n;
 
-        int left = 0, right = cols - 1, up = 0, down = rows - 1;
+        int left = 0, right = n - 1, up = 0, down = n - 1;
         pair<int, int> curPos = {0, 0};
         vector<pair<int, int>> direction;
         direction.push_back({0, 1});
@@ -24,9 +23,11 @@ public:
         direction.push_back({0, -1});
         direction.push_back({-1, 0});
         int directionIdx = 0;
-        while (result.size() < count) {
+        int curNum = 1;
+        while (curNum <= count) {
             while(curPos.first >= up && curPos.first <= down && curPos.second >= left && curPos.second <= right) {
-                result.push_back(matrix[curPos.first][curPos.second]);
+                result[curPos.first][curPos.second] = curNum;
+                ++curNum;
                 curPos.first += direction[directionIdx].first;
                 curPos.second += direction[directionIdx].second;
             }
