5a9e82d0db6c32b503b46624e94e0c424fcc6faa
YanZhao <whuhenry@hotmail.com>
2023-01-30 10:25:10 +0800

    完成2236

---
+++ 2236.判断根结点是否等于子结点之和.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=2236 lang=python3
+#
+# [2236] 判断根结点是否等于子结点之和
+#
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        return root.val == root.left.val + root.right.val
+# @lc code=end
+
